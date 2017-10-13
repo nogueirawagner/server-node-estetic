ccb7d2b30e81e71e6e115be5041e37a3f09dd461
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-10-13 16:26:40 -0300

    criando metodos no controller example

--- src/controllers/example-controller.js
+++ src/controllers/example-controller.js
@@ -2,6 +2,44 @@
 
 const repositorio = require('./example-repositorio');
 
-exports.NewExample = async (req, res) =>{
-    
+exports.NewExample = async (req, res) => {
+    try {
+        const data = repositorio.NewExample(req.body);
+        res.status(200).send({
+            message: 'Example cadastrado.'
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao cadastrar',
+            data: e
+        });
+    }
+}
+
+exports.UpdateExample = async (req, res) => {
+    try {
+        const data = repositorio.UpdateExample(req.params.id, req.body);
+        res.status(200).send({
+            message: 'Example alterado.'
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao alterar',
+            data: e
+        });
+    }
+}
+
+exports.RemoveExample = async (req, res) => {
+    try {
+        const data = repositorio.RemoveExample(req.params.id);
+        res.status(200).send({
+            message: 'Example removido'
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao remover',
+            data: e
+        });
+    }
 }

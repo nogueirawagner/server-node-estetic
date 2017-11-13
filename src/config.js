f64e348064ae83201b535d91b642f28238ec9ab0
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-11-13 08:39:29 -0200

    Alterando base local

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'e1afe3f0-8a96-4560-9ef0-78cb248fdd6f'
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo.'
 
 module.exports = {
-    connectionString: 'mongodb://sa:sa@ds042607.mlab.com:42607/capturadados',
+    connectionString: 'mongodb://sa:sa@ds042607.mlab.com:42607/owc-local',
     sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
